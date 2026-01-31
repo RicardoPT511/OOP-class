39d9919c93beadeccc6fbbf1c1ec49370e8df68c
Ricardo <rex_pt123@icloud.com>
2026-01-30 20:08:47 -0600

    Primer commit proyecto C#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿            
+
+ float b,h,a ;
+ Console.WriteLine("Ingrese los datos de su rectangulo (base y altura)");
+ Console.WriteLine("Altura :");
+ h = float.Parse(Console.ReadLine());
+ Console.WriteLine("Base :");
+ b = float.Parse(Console.ReadLine());
+
+ a = b * h;
+ Console.WriteLine("El area rsultante es :" +a);
+ 
